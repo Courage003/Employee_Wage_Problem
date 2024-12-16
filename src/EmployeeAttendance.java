ce1c3451aa00005dc797a1f439315c833ade15bb
Courage003 <rastogidhruv28@gmail.com>
2024-12-16 15:03:19 +0530

    Solving using Switch Case statement

--- src/EmployeeAttendance.java
+++ src/EmployeeAttendance.java
@@ -1,21 +1,24 @@
 public class EmployeeAttendance {
+    private static final int wage_per_hour=20;
+    private static final int full_time=1;
+    private static final int part_time=2;
     public static void main(String[] args){
         System.out.println("Welcome to WageComputation Program: ");
-        int full_time=1;
-        int part_time=2;
+
 
         int empCheck=(int)(Math.random()*3);
-        int wage_per_hour=20;
+
         int empHours=0;
 
-        if(empCheck==full_time){
-            empHours=8;
-        }
-        else if(empCheck==part_time){
-            empHours=4;
-        }
-        else{
-            empHours=0;
+        switch (empCheck) {
+            case full_time:
+                empHours = 8;
+                break;
+            case part_time:
+                empHours = 4;
+                break;
+            default:
+                empHours = 0;
         }
         int dailyWage = wage_per_hour*empHours;
         System.out.println("Employee worked for " + empHours + " hours. Daily Wage: $" + dailyWage);
