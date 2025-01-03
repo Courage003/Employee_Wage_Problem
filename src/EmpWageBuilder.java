5ad8cd076c6e0ad5065def9ca777bc0824e611d9
Courage003 <rastogidhruv28@gmail.com>
2025-01-03 21:38:08 +0530

    Storing daily wage along with total wage

--- src/EmpWageBuilder.java
+++ src/EmpWageBuilder.java
@@ -1,13 +1,14 @@
 import java.util.ArrayList;
 import java.util.List;
 
-class EmpWageBuilder implements IEmployeeWage{
-    private static class Company{
+class EmpWageBuilder implements IEmployeeWage {
+    private static class Company {
         String name;
         int wagePerHour;
         int maxWorkingDays;
         int maxWorkingHours;
         int totalWage;
+        List<Integer> dailyWages; // List to store daily wages
 
         Company(String name, int wagePerHour, int maxWorkingDays, int maxWorkingHours) {
             this.name = name;
@@ -15,49 +16,43 @@ class EmpWageBuilder implements IEmployeeWage{
             this.maxWorkingDays = maxWorkingDays;
             this.maxWorkingHours = maxWorkingHours;
             this.totalWage = 0;
-        }
-
-        @Override
-        public String toString() {
-            return "Company{" +
-                    "totalWage=" + totalWage +
-                    ", maxWorkingHours=" + maxWorkingHours +
-                    ", maxWorkingDays=" + maxWorkingDays +
-                    ", wagePerHour=" + wagePerHour +
-                    ", name='" + name + '\'' +
-                    '}';
+            this.dailyWages = new ArrayList<>();
         }
     }
 
-    private final List<Company> companies= new ArrayList<>();
+    private final List<Company> companies = new ArrayList<>();
 
     @Override
-    public void addCompany(String companyName, int wagePerHour, int maxWorkingDays, int maxWorkingHours){
-        companies.add(new Company(companyName,wagePerHour,maxWorkingDays,maxWorkingHours));
+    public void addCompany(String companyName, int wagePerHour, int maxWorkingDays, int maxWorkingHours) {
+        companies.add(new Company(companyName, wagePerHour, maxWorkingDays, maxWorkingHours));
     }
 
     @Override
-    public void computeWages(){
-        for(Company company: companies){
-            company.totalWage=computeWageForCompany(company);
+    public void computeWages() {
+        for (Company company : companies) {
+            company.totalWage = computeWageForCompany(company);
         }
     }
 
-    private int computeWageForCompany(Company company){
-        int totalHours=0, totalDays=0;
-        while(totalHours<=company.maxWorkingHours && totalDays< company.maxWorkingDays){
-            int hoursWorked= (int)(Math.random()*10)%9; //Random hours (0-8)
-            totalHours+=hoursWorked;
+    private int computeWageForCompany(Company company) {
+        int totalHours = 0, totalDays = 0, dailyWage;
+        while (totalHours <= company.maxWorkingHours && totalDays < company.maxWorkingDays) {
+            int hoursWorked = (int) (Math.random() * 10) % 9; // Random hours (0-8)
+            dailyWage = hoursWorked * company.wagePerHour;
+            company.dailyWages.add(dailyWage); // Store daily wage
+            totalHours += hoursWorked;
             totalDays++;
         }
-        return totalHours*company.wagePerHour;
+        return totalHours * company.wagePerHour;
     }
 
     @Override
-    public void displayAllCompanyWages(){
-        for(Company company: companies){
-            System.out.println("Total Wage for " + company.name + ": " + company.totalWage);
+    public void displayAllCompanyWages() {
+        for (Company company : companies) {
+            System.out.println("Company: " + company.name);
+            System.out.println("Daily Wages: " + company.dailyWages);
+            System.out.println("Total Wage: " + company.totalWage);
+            System.out.println();
         }
     }
 }
-
